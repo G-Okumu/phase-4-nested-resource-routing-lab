61da5022f74c98d7aa27cd9e4a7e4b7e77815661
George-Okumu <gokumu12@gmail.com>
2022-06-20 00:04:22 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    # items = Item.all
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    item = Item.create(items_params)
+    render json: item, status: :created
+  end
+  private
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
 end
